7fed64c038386ec4db22e35050fd643427cdd7f8
Simone Soares <simonesoldeprimavera@gmail.com>
2024-06-28 21:17:26 -0300

    média ponderada

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+let n1 = 6;
+let n2 = 8;
+let n3 = 10;
+
+let p1 = 2;  
+let p2 = 3;  
+let p3 = 5;  
+
+// Passo 2: Calcular a média ponderada
+let media = ((n1 * p1) + (n2 * p2) + (n3 * p3)) / (p1 + p2 + p3);
+
+
+console.log("A média ponderada é: " + media);
